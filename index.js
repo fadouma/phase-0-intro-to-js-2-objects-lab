a0a0f25bf78de3d76ee91e8c926952a531759fb3
fadouma ahmed <fannyahmed85@gmail.com>
2024-06-24 11:49:42 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+const employee = {name : "Fanny", streetAddress : "14400 E Fremont Ave"}
+function updateEmployeeWithKeyAndValue(employee, key,value){
+    const copy = {...employee}
+    copy[key] = value;
+    return copy;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key,value){
+    employee[key] = value
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key){
+    const clone = {...employee}
+    delete clone [key]
+    return clone
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
